01b662d3b0ef3a954696a47cfb525454290ccde4
Bert Allen <mrlaughingdragon@aol.com>
2016-11-02 13:05:21 -0600

    initial

--- components/dashboard.js
+++ components/dashboard.js
@@ -15,17 +15,44 @@
 		var db = this;
 
 		var data = dataService.getData();
+		 
+		function sale(catagory) {
+			return "$" + (catagory / 1000000).toFixed(2) + "M";
+		}
+
+		function saleProgress(catagory) {
+			return Math.round(catagory / 5000000 * 100);
+		}
 		
 		var sales = data.reduce(function (total, item) {
 			return total + parseFloat(item.sales.replace('$', ''));
 		}, 0);
 
-		db.sales = '$' + (sales / 1000000).toFixed(2) + 'M';
-		db.salesProgress = Math.round(sales / 5000000 * 100);
+		// db.sales = '$' + (sales / 1000000).toFixed(2) + 'M';
+		db.sales = sale(sales);
+		db.salesProgress = saleProgress(sales);
 
-		// calculate sales by men		
+		// calculate sales by men
+		let men = data.reduce(function (total, item) {
+			if (item.gender == "Male") {
+				total += parseFloat(item.sales.replace("$", ""))
+			}
+			return total;
+		}, 0);
+
+		db.salesByMen = sale(men);
+		db.salesProgByMen = saleProgress(men)
 
 		// calculate sales by women
+		let women = data.reduce(function (total, item) {
+			if (item.gender == "Female") {
+				total += parseFloat(item.sales.replace("$", ""))
+			}
+			return total;
+		}, 0);
+
+		db.salesByWomen = sale(women);
+		db.salesProgByWomen = saleProgress(women)
 		
 		// calculate sales by top state
 
